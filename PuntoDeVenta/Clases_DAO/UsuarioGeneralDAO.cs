b9121e00fe1976763537d470d3988653dd636865
Edgar Elihu Garcia Ramirez <edgar.elihu.garcia.ramirez@gmail.com>
2018-10-26 21:42:33 -0500

    Adición de clases DAO para implementar las funciones CRUD

---
+++ PuntoDeVenta/Clases_DAO/UsuarioGeneralDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoDeVenta.Clases_DAO
+{
+    class UsuarioGeneralDAO : ICRUD<UsuarioGeneral>
+    {
+        public int crear(UsuarioGeneral t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool actualizar(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool eliminar(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object leerPorCondicion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsuarioGeneral leerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UsuarioGeneral> leerTodo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PuntoDeVenta/Clases_DAO/VentasDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoDeVenta.Clases_DAO
+{
+    class VentasDAO : ICRUD<Venta>
+    {
+        public int crear(Venta t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool actualizar(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool eliminar(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object leerPorCondicion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Venta leerPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Venta> leerTodo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
